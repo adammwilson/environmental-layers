f042de0fe7aaa2897faf4315a7a60bdde0fc5122
Benoit Parmentier <parmentier@nceas.ucsb.edu>
2012-05-01 16:10:15 -0700

    GAM LST, added model with forest only term -364 dates assessment, task #406

--- climate/research/oregon/interpolation/GAM_LST_reg.R
+++ climate/research/oregon/interpolation/GAM_LST_reg.R
@@ -18,12 +18,13 @@ library(rgdal)
 ###Parameters and arguments
 
 infile1<-"ghcn_or_tmax_b_04142012_OR83M.shp"
-#path<-"/data/computer/parmentier/Data/IPLANT_project/data_Oregon_stations"
-path<-"H:/Data/IPLANT_project/data_Oregon_stations"
+path<-"/data/computer/parmentier/Data/IPLANT_project/data_Oregon_stations"
+#path<-"H:/Data/IPLANT_project/data_Oregon_stations"
 setwd(path) 
 infile2<-"dates_interpolation_03052012.txt"                                          #List of 10 dates for the regression
+infile2<-"list_365_dates_04212012.txt"
 prop<-0.3                                                                            #Proportion of testing retained for validation   
-out_prefix<-"_04252012_LST_residuals"
+out_prefix<-"_05012012_mod8_LST"
 infile3<-"LST_dates_var_names.txt"
 infile4<-"models_interpolation_04032012b.txt"
 
@@ -33,8 +34,8 @@ infile4<-"models_interpolation_04032012b.txt"
 filename<-sub(".shp","",infile1)              #Removing the extension from file.
 ghcn<-readOGR(".", filename)                  #reading shapefile 
 
-CRS<-proj4string(ghcn)
-
+proj4string(ghcn) #This retrieves the coordinate system for the SDF
+CRS_ghcn<-proj4string(ghcn) #this can be assigned to mean_LST!!!
 
 ghcn = transform(ghcn,Northness = cos(ASPECT)) #Adding a variable to the dataframe
 ghcn = transform(ghcn,Eastness = sin(ASPECT))  #adding variable to the dataframe.
@@ -46,22 +47,21 @@ dates <-readLines(paste(path,"/",infile2, sep=""))
 LST_dates <-readLines(paste(path,"/",infile3, sep=""))
 models <-readLines(paste(path,"/",infile4, sep=""))
 
-results <- matrix(1,length(dates),14)            #This is a matrix containing the diagnostic measures from the GAM models.
+results <- matrix(1,length(dates),15)            #This is a matrix containing the diagnostic measures from the GAM models.
 
-results_AIC<- matrix(1,length(dates),length(models)+2)  
-results_GCV<- matrix(1,length(dates),length(models)+2)
-results_DEV<- matrix(1,length(dates),length(models)+2)
-results_RMSE<- matrix(1,length(dates),length(models)+2)
-cor_LST_LC1<-matrix(1,10,1)      #correlation LST-LC1
-cor_LST_LC3<-matrix(1,10,1)      #correlation LST-LC3
-cor_LST_tmax<-matrix(1,10,1)    #correlation LST-tmax
+results_AIC<- matrix(1,length(dates),length(models)+3)  
+results_GCV<- matrix(1,length(dates),length(models)+3)
+results_DEV<- matrix(1,length(dates),length(models)+3)
+results_RMSE<- matrix(1,length(dates),length(models)+3)
+cor_LST_LC1<-matrix(1,length(dates),1)      #correlation LST-LC1
+cor_LST_LC3<-matrix(1,length(dates),1)      #correlation LST-LC3
+cor_LST_tmax<-matrix(1,length(dates),1)    #correlation LST-tmax
 #Screening for bad values
 
 ghcn_all<-ghcn
 ghcn_test<-subset(ghcn,ghcn$tmax>-150 & ghcn$tmax<400)
 ghcn_test2<-subset(ghcn_test,ghcn_test$ELEV_SRTM>0)
 ghcn<-ghcn_test2
-#coords<- ghcn[,c('x_OR83M','y_OR83M')]
 
 month_var<-c("mm_01","mm_02","mm_03","mm_04","mm_05","mm_06","mm_07","mm_08","mm_09", "mm_10", "mm_11", "mm_12")
 ghcn.subsets <-lapply(dates, function(d) subset(ghcn, date==d)) #this creates a list of 10 subsets data
@@ -94,12 +94,14 @@ for(i in 1:length(dates)){            # start of the for loop #1
   ####Regression part 2: GAM models
 
   mod1<- gam(tmax~ s(lat) + s (lon) + s (ELEV_SRTM), data=data_s)
-  mod2<- gam(tmax~ s(lat,lon,ELEV_SRTM), data=data_s)
+  #mod2<- gam(tmax~ s(lat,lon,ELEV_SRTM), data=data_s)
+  mod2<- gam(tmax~ s(lat,lon) + s(ELEV_SRTM), data=data_s)
   mod3<- gam(tmax~ s(lat) + s (lon) + s (ELEV_SRTM) +  s (Northness)+ s (Eastness) + s(DISTOC), data=data_s)
   mod4<- gam(tmax~ s(lat) + s (lon) + s(ELEV_SRTM) + s(Northness) + s (Eastness) + s(DISTOC) + s(LST), data=data_s)
   mod5<- gam(tmax~ s(lat,lon) +s(ELEV_SRTM) + s(Northness,Eastness) + s(DISTOC) + s(LST), data=data_s)
   mod6<- gam(tmax~ s(lat,lon) +s(ELEV_SRTM) + s(Northness,Eastness) + s(DISTOC) + s(LST,LC1), data=data_s)
   mod7<- gam(tmax~ s(lat,lon) +s(ELEV_SRTM) + s(Northness,Eastness) + s(DISTOC) + s(LST,LC3), data=data_s)
+  mod8<- gam(tmax~ s(lat,lon) +s(ELEV_SRTM) + s(Northness,Eastness) + s(DISTOC) + s(LST) + s(LC1), data=data_s)
   
   ####Regression part 3: Calculating and storing diagnostic measures
   results_AIC[i,1]<- dates[i]  #storing the interpolation dates in the first column
@@ -111,6 +113,7 @@ for(i in 1:length(dates)){            # start of the for loop #1
   results_AIC[i,7]<- AIC (mod5)
   results_AIC[i,8]<- AIC (mod6)
   results_AIC[i,9]<- AIC (mod7)
+  results_AIC[i,10]<- AIC (mod8)
   
   results_GCV[i,1]<- dates[i]  #storing the interpolation dates in the first column
   results_GCV[i,2]<- ns        #number of stations used in the training stage
@@ -121,6 +124,7 @@ for(i in 1:length(dates)){            # start of the for loop #1
   results_GCV[i,7]<- mod5$gcv.ubre
   results_GCV[i,8]<- mod6$gcv.ubre
   results_GCV[i,9]<- mod7$gcv.ubre
+  results_GCV[i,10]<- mod7$gcv.ubre
   
   results_DEV[i,1]<- dates[i]  #storing the interpolation dates in the first column
   results_DEV[i,2]<- ns        #number of stations used in the training stage
@@ -130,7 +134,8 @@ for(i in 1:length(dates)){            # start of the for loop #1
   results_DEV[i,6]<- mod4$deviance
   results_DEV[i,7]<- mod5$deviance
   results_DEV[i,8]<- mod6$deviance
-  results_DEV[i,9]<- mod6$deviance
+  results_DEV[i,9]<- mod7$deviance
+  results_DEV[i,10]<- mod8$deviance
   
   #####VALIDATION: Prediction checking the results using the testing data########
  
@@ -141,6 +146,7 @@ for(i in 1:length(dates)){            # start of the for loop #1
   y_mod5<- predict(mod5, newdata=data_v, se.fit = TRUE) 
   y_mod6<- predict(mod6, newdata=data_v, se.fit = TRUE)
   y_mod7<- predict(mod7, newdata=data_v, se.fit = TRUE)
+  y_mod8<- predict(mod8, newdata=data_v, se.fit = TRUE)
   
   res_mod1<- data_v$tmax - y_mod1$fit #Residuals for GMA model that resembles the ANUSPLIN interpolation
   res_mod2<- data_v$tmax - y_mod2$fit   #Residuals for GAM model that resembles the PRISM interpolation                               
@@ -149,6 +155,7 @@ for(i in 1:length(dates)){            # start of the for loop #1
   res_mod5<- data_v$tmax - y_mod5$fit
   res_mod6<- data_v$tmax - y_mod6$fit
   res_mod7<- data_v$tmax - y_mod7$fit
+  res_mod8<- data_v$tmax - y_mod8$fit
   
   RMSE_mod1 <- sqrt(sum(res_mod1^2)/nv)          
   RMSE_mod2 <- sqrt(sum(res_mod2^2)/nv)
@@ -157,6 +164,7 @@ for(i in 1:length(dates)){            # start of the for loop #1
   RMSE_mod5 <- sqrt(sum(res_mod5^2)/nv)
   RMSE_mod6 <- sqrt(sum(res_mod6^2)/nv)
   RMSE_mod7 <- sqrt(sum(res_mod7^2)/nv)
+  RMSE_mod8 <- sqrt(sum(res_mod8^2)/nv)
 
   results_RMSE[i,1]<- dates[i]  #storing the interpolation dates in the first column
   results_RMSE[i,2]<- ns        #number of stations used in the training stage
@@ -167,62 +175,20 @@ for(i in 1:length(dates)){            # start of the for loop #1
   results_RMSE[i,7]<- RMSE_mod5
   results_RMSE[i,8]<- RMSE_mod6
   results_RMSE[i,9]<- RMSE_mod7
+  results_RMSE[i,10]<- RMSE_mod8
   
-  #Saving dataset in dataframes: residuals from RMSE
-  
-#   data_v$mod1<-y_mod1$fit
-#   data_v$mod2<-y_mod2$fit
-#   data_v$mod3<-y_mod3$fit
-#   data_v$mod4<-y_mod4$fit
-#   data_v$mod5<-y_mod5$fit
-#   data_v$mod6<-y_mod6$fit
-#   data_v$mod7<-y_mod7$fit
-#   
-#   data_s$mod1<-mod1$fit
-#   data_s$mod2<-mod2$fit
-#   data_s$mod3<-mod3$fit
-#   data_s$mod4<-mod4$fit
-#   data_s$mod5<-mod5$fit
-#   data_s$mod6<-mod6$fit
-#   data_s$mod7<-mod7$fit
-#   
-  data_v$res_mod1<-as.numeric(res_mod1)
-  data_v$res_mod2<-as.numeric(res_mod2)
-  data_v$res_mod3<-as.numeric(res_mod3)
-  data_v$res_mod4<-as.numeric(res_mod4)
-  data_v$res_mod5<-as.numeric(res_mod5)
-  data_v$res_mod6<-as.numeric(res_mod6)
-  data_v$res_mod7<-as.numeric(res_mod7)
-  
-  data_s$res_mod1<-as.numeric(mod1$residuals)
-  data_s$res_mod2<-as.numeric(mod2$residuals)
-  data_s$res_mod3<-as.numeric(mod3$residuals)
-  data_s$res_mod4<-as.numeric(mod4$residuals)
-  data_s$res_mod5<-as.numeric(mod5$residuals)
-  data_s$res_mod6<-as.numeric(mod6$residuals)
-  data_s$res_mod7<-as.numeric(mod7$residuals)
-  
+  #Saving dataset in dataframes
   data_name<-paste("ghcn_v_",dates[[i]],sep="")
   assign(data_name,data_v)
-  write.table(data_v, file= paste(path,"/",data_name,".txt",sep=""), sep=" ")
-  #write out a new shapefile (including .prj component)
-  coords<- data_v[,c('x_OR83M','y_OR83M')]
-  coordinates(data_v)<-coords
-  proj4string(data_v)<-CRS  #Need to assign coordinates...
-  outfile<-sub(".shp","",data_name)   #Removing extension if it is present
-  writeOGR(data_v,".", outfile, driver ="ESRI Shapefile")
-  
   data_name<-paste("ghcn_s_",dates[[i]],sep="")
   assign(data_name,data_s)
-  write.table(data_s, file= paste(path,"/",data_name,".txt",sep=""), sep=" ")
-  #write out a new shapefile (including .prj component)
-  coords<- data_s[,c('x_OR83M','y_OR83M')]
-  coordinates(data_s)<-coords
-  proj4string(data_s)<-CRS  #Need to assign coordinates..
-  outfile<-sub(".shp","",data_name)   #Removing extension if it is present
-  writeOGR(data_s,".", outfile, driver ="ESRI Shapefile")
+  #ghcn_v<-ls(pattern="ghcn_v_")
   
-  # end of the for loop #1
+
+  cor_LST_LC1[i]<-cor(ghcn.subsets[[i]]$LST,ghcn.subsets[[i]]$LC1)
+  cor_LST_LC3[i]<-cor(ghcn.subsets[[i]]$LST,ghcn.subsets[[i]]$LC3)
+  
+  #end of the for loop #1
   }
 
 ## Plotting and saving diagnostic measures
@@ -250,7 +216,6 @@ write.table(results_table_AIC, file= paste(path,"/","results_GAM_Assessment",out
 f<-file(paste(path,"/","results_GAM_Assessment",out_prefix,"2.txt",sep=""),"w")
 write(results_table_RMSE, file=f)
 close(f)
-
 # End of script##########
 
 #Selecting dates and files based on names
@@ -262,3 +227,4 @@ close(f)
 #   cor_LST_LC3[i]<-cor(ghcn.subsets[[i]]$LST,ghcn.subsets[[i]]$LC3)
 # }
 
+#mod9<- gam(tmax~ te(lat,lon,ELEV_SRTM), data=ghcn_s_20101031)
