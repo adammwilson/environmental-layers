b7b786b1378ec6a9ff5b969b20cdbfed816b4f17
Adam M. Wilson <adam.wilson@yale.edu>
2012-06-14 15:28:15 -0400

    Minor updates to dataset size calculation

--- climate/extra/dailydatasize.r
+++ climate/extra/dailydatasize.r
@@ -10,7 +10,7 @@ n.globe=510072000 #km2, from http://en.wikipedia.org/wiki/Earth
 ## Land Area
 n.land=148940000 #km2, from http://en.wikipedia.org/wiki/Earth
 
-## total values
+## total values (space x time)
 n.landtime=n.dates*n.land
 n.globetime=n.dates*n.globe
 
@@ -22,16 +22,17 @@ formatC(n.landtime, format="f",digits=0, big.mark=',')
 
 ### approximate storage size for various data types
 types=as.data.frame(matrix(c(
-  "Short integer",-32768, 32767,2,
-  "Long integer", -2147483648,2147483647,4,
-  "Single-precision floating-point", -3.4e38,1.2e38,4,
-  "Double-precision floating-point", -2.2e308,1.8e308,8
-                         ),ncol=4,byrow=T,dimnames=list(1:4,c("type","min","max","bytes"))),stringsAsFactors=F)
+  "short int","Short integer",-32768, 32767,2,
+  "long int","Long integer", -2147483648,2147483647,4,
+  "single","Single-precision floating-point", -3.4e38,1.2e38,4,
+  "double","Double-precision floating-point", -2.2e308,1.8e308,8
+                         ),ncol=5,byrow=T,dimnames=list(1:4,c("type","longtype","min","max","bytes"))),stringsAsFactors=F)
 types$min=as.numeric(types$min);types$max=as.numeric(types$max);types$bytes=as.numeric(types$bytes)
 types
 
-### now estimate TB storage for total values (not full grid, just the values)
+### now estimate TB storage for total values
 
-types$storage.landtime.TB=n.landtime*types$bytes/2^40
-types$storage.globetime.TB=n.globetime*types$bytes/2^40; types
+types$storage.landtime.TB=round(n.landtime*types$bytes/2^40,1)
+types$storage.globetime.TB=round(n.globetime*types$bytes/2^40,1); types
 
+types[,-c(2:4)]
