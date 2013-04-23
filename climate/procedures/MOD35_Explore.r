b3f5074e778fa11b8a78b394f4e1d8687ee3a065
Adam M. Wilson <adam.wilson@yale.edu>
2013-04-23 16:29:43 -0400

    Added initial code to make KML of cloud climatologies

--- climate/procedures/MOD35_Explore.r
+++ climate/procedures/MOD35_Explore.r
@@ -1,5 +1,5 @@
 ## explore the MOD35 data downloaded and gridded by the DAAC
-setwd("~/acrobates/projects/interp/data/modis/mod35")
+setwd("~/acrobates/adamw/projects/interp/data/modis/mod35")
 
 library(raster)
 library(rasterVis)
@@ -29,8 +29,10 @@ library(rgdal)
 ## get % cloudy
 v5=stack(brick("../mod06/summary/MOD06_h11v08_ymoncld01.nc",varname="CLD01"))
 projection(v5)="+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs"
-v6=stack(brick("MOD35_h11v08.nc",varname="CLD01"))
+v6=stack(brick("summary/MOD35_h11v08.nc",varname="PCloud"))
 projection(v6)="+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs"
+v6=setZ(v6,as.Date(paste("2011-",1:12,"-15",sep="")))
+names(v6)=month.name
 
 ## generate means
 v6m=mean(v6)
@@ -38,7 +40,7 @@ v5m=mean(v5)
 
 
 ## landcover
-lulc=raster("~/acrobatesroot/Data/environ/global/landcover/MODIS/MCD12Q1_IGBP_2005_v51.tif")
+lulc=raster("~/acrobatesroot/jetzlab/Data/environ/global/landcover/MODIS/MCD12Q1_IGBP_2005_v51.tif")
 projection(lulc)="+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs"
 lulc=crop(lulc,v6)
 
@@ -59,7 +61,6 @@ lulc_levels2=c("Water","Forest","Forest","Forest","Forest","Forest","Shrublands"
 levels(lulcf)=list(data.frame(ID=0:16,LULC=lulc_levels,LULC2=lulc_levels2))
 
 
-
 ### load WORLDCLIM elevation 
 dem=raster("../../tiles/h11v08/dem_h11v08.tif",format="GTiff")
 projection(dem)="+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs"
@@ -68,16 +69,14 @@ dif=v6-v5
 names(dif)=month.name
 
 difm=v6m-v5m
+v5v6compare=stack(v5m,v6m,difm)
+names(v5v6compare)=c("Collection 5","Collection 6","Difference (C6-C5)")
 
 tile=extent(v6)
 
 ### compare differences between v5 and v6 by landcover type
 lulcm=as.matrix(lulc)
 forest=lulcm>=1&lulcm<=5
-
-
-boxplot(cld)
-splom(cld)
 
 
 #####################################
@@ -104,6 +103,8 @@ mod43qc[mod43qc<0|mod43qc>100]=NA
 modprod=stack(mod17qc,mod43qc)
 names(modprod)=c("MOD17","MOD43")
 
+###
+
 n=100
 at=seq(0,100,len=n)
 cols=grey(seq(0,1,len=n))
@@ -121,16 +122,15 @@ names(mcompare)=c("Collection_5","Collection_6")
 names(mdiff)=c("Collection_5-Collection_6")
 
 
-pdf("output/mod35compare.pdf",width=11,height=8.5)
+CairoPDF("output/mod35compare.pdf",width=11,height=8.5)
+#CairoPNG("output/mod35compare_%d.png",units="in", width=11,height=8.5,pointsize=4000,dpi=1200,antialias="subpixel")
 
 levelplot(mcompare,col.regions=cols,at=at,margin=F,sub="Frequency of MOD35 Clouds in March")
-levelplot(dif,col.regions=bgyr(20),margin=F)
-levelplot(mdiff,col.regions=bgyr(20),margin=F)
+#levelplot(dif,col.regions=bgyr(20),margin=F)
+levelplot(mdiff,col.regions=bgyr(100),at=seq(mdiff@data@min,mdiff@data@max,len=100),margin=F)
 
 
 boxplot(as.matrix(subset(dif,subset=1))~forest,varwidth=T,notch=T);abline(h=0)
-
-dev.off()
 
 
 levelplot(modprod,main="Missing Data (%) in MOD17 (NPP) and MOD43 (BRDF Reflectance)",
@@ -144,9 +144,21 @@ levelplot(v5m,main="Missing Data (%) in MOD17 (NPP) and MOD43 (BRDF Reflectance)
           sub="Tile H11v08 (Venezuela)",col.regions=cols,at=at,
           xlim=c(-7200000,-6670000),ylim=c(0,400000),margin=F)
 
-levelplot(stack(v5m,v6m),main="Missing Data (%) in MOD17 (NPP) and MOD43 (BRDF Reflectance)",
+
+levelplot(subset(v5v6compare,1:2),main="Proportion Cloudy Days (%) in Collection 5 and 6 MOD35",
+          sub="Tile H11v08 (Venezuela)",col.regions=cols,at=at,
+          margin=F)
+
+levelplot(subset(v5v6compare,1:2),main="Proportion Cloudy Days (%) in Collection 5 and 6 MOD35",
           sub="Tile H11v08 (Venezuela)",col.regions=cols,at=at,
           xlim=c(-7200000,-6670000),ylim=c(0,400000),margin=F)
+
+levelplot(subset(v5v6compare,1:2),main="Proportion Cloudy Days (%) in Collection 5 and 6 MOD35",
+          sub="Tile H11v08 (Venezuela)",col.regions=cols,at=at,
+          xlim=c(-7500000,-7200000),ylim=c(700000,1000000),margin=F)
+
+
+dev.off()
 
 ### smoothing plots
 ## explore smoothed version
@@ -168,3 +180,34 @@ tile_ll=projectExtent(v6, "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
 
 62,59
 0,3
+
+
+
+#### export KML timeseries
+library(plotKML)
+tile="h11v08"
+file=paste("summary/MOD35_",tile,".nc",sep="")
+system(paste("gdalwarp -overwrite -multi -ot INT16 -r cubicspline -srcnodata 255 -dstnodata 255 -s_srs '+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs' -t_srs 'EPSG:4326' NETCDF:",file,":PCloud  MOD35_",tile,".tif",sep=""))
+
+v6sp=brick(paste("MOD35_",tile,".tif",sep=""))
+v6sp=readAll(v6sp)
+
+## wasn't working with line below, perhaps Z should just be text? not date?
+v6sp=setZ(v6sp,as.Date(paste("2011-",1:12,"-15",sep="")))
+names(v6sp)=month.name
+
+kml_open("output/mod35.kml")
+
+
+kml_layer.RasterBrick(v6sp,
+     plot.legend = TRUE, dtime = "", tz = "GMT",
+    z.lim = c(0,100),colour_scale = get("colour_scale_numeric", envir = plotKML.opts))
+#    home_url = get("home_url", envir = plotKML.opts),
+#    metadata = NULL, html.table = NULL,
+#    altitudeMode = "clampToGround", balloon = FALSE,
+)
+
+logo = "http://static.tumblr.com/t0afs9f/KWTm94tpm/yale_logo.png"
+kml_screen(image.file = logo, position = "UL", sname = "YALE logo",size=c(.1,.1))
+kml_close("mod35.kml")
+kml_compress("mod35.kml",files=c(paste(month.name,".png",sep=""),"obj_legend.png"),zip="/usr/bin/zip")
