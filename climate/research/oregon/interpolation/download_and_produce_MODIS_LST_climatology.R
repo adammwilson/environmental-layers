f37cc8b74e30f1e4614cd6b4830260e74f12218a
Benoit Parmentier <parmentier@nceas.ucsb.edu>
2013-05-30 10:45:00 -0700

    LST clim averages calc,testing parallel call to python scripts

--- climate/research/oregon/interpolation/download_and_produce_MODIS_LST_climatology.R
+++ climate/research/oregon/interpolation/download_and_produce_MODIS_LST_climatology.R
@@ -7,9 +7,11 @@
 # This R script calls python scripts. 
 #
 #AUTHORS: Benoit Parmentier    
-#         based on modified earlier python scripts from J. Regetz
-#DATE: 05/29/2013                                                                                 
-
+#         Using python scripts started by J. Regetz and modified by B.Parmentier.
+#DATE: 05/30/2013                                                                                 
+#To Do:
+#-collect list of LST files created
+#-solve issue realted to parallelization of python script: GRASS problem?
 #PROJECT: NCEAS INPLANT: Environment and Organisms --TASK#???--   
 
 ##################################################################################################
@@ -18,7 +20,7 @@ download_calculate_MODIS_LST_climatology <-function(j,list_param){
   
   ###Function to download and calculate LST climatology from MODIS tiles
   #AUTHOR: Benoit Parmentier                                                                       
-  #DATE: 05/29/2013                                                                                 
+  #DATE: 05/30/2013                                                                                 
   #PROJECT: NCEAS INPLANT: Environment and Organisms --TASK#363--   
   
   #1) var 
@@ -53,8 +55,8 @@ download_calculate_MODIS_LST_climatology <-function(j,list_param){
   #j to be used later...parallelization??
   
   #Additional parameters
-  #list_tiles_modis <- unlist(strsplit(list_tiles_modis,","))  # transform string into separate element in char vector
-  #list_tiles_modis <- list_tiles_modis[j]
+  list_tiles_modis <- unlist(strsplit(list_tiles_modis,","))  # transform string into separate element in char vector
+  list_tiles_modis <- list_tiles_modis[j]
   end_month= "12"
   start_month= "1"
   
@@ -84,71 +86,75 @@ download_calculate_MODIS_LST_climatology <-function(j,list_param){
   
   #command_str<-"python /home/parmentier/Data/test5.py h01v05,h02v05 2001 2005 12 1 /benoit 1 test_out 1"
   #paste(toString(list_param_python_script),collapse=",",sep=" ")
+  command_download_str <- paste("python",modis_download_script, list_param_python_script_str,sep=" ")
+  command_clim_str <- paste("python",clim_script, list_param_python_script_str,sep=" ") #preparing shell command
   
   ##Downownload files if necessary...
   if (download==1){
-    command_download_str <- paste("python",modis_download_script, list_param_python_script_str,sep=" ")
-    #command_str <- paste("python","/home/parmentier/Data/benoit_test/test5.py", list_param_python_script_str,sep=" ")
     system(command_download_str)
   }
   
   if (clim_calc==1){
     ##Now run climatology: Can add other climatology scripts or method to produce climatology later on...
-    source(grass_setting_script) #Setting to access GRASS (current setting valid for Atlas at nceas)
-    command_clim_str <- paste("python",clim_script, list_param_python_script_str,sep=" ") #preparing shell command
+    source(grass_setting_script) #Setting to access GRASS (current setting valid for Atlas at nceas)  
     system(command_clim_str)
-    #/home/parmentier/Data/IPLANT_project/Venezuela_interpolation/Venezuela_01142013/climatology_05132013.py
   }
   
-  ## return list of files???, may be modified later to return clim list and list of downloaded files + missing.
-  if (clim_calc==1){
-    list_output_obj <-list(command_download_str,command_clim_str)
-    names(list_output_obj) <- c("command_download_str", "command_clim_str")
-  } else{
-    list_output_obj <-list(command_download_str)
-    names(list_output_obj) <- c("command_download_str")
-  }
-    #Add function to list files created or modify in python...
+#   ## return list of files???, may be modified later to return clim list and list of downloaded files + missing.
+#   if (clim_calc==1){
+#     list_output_obj <-list(command_download_str,command_clim_str)
+#     names(list_output_obj) <- c("command_download_str", "command_clim_str")
+#   } else{
+#     list_output_obj <-list(command_download_str)
+#     names(list_output_obj) <- c("command_download_str")
+#   }
+  #Add function to list files created or modify in python...
   
+  list_output_obj <-list(command_download_str,command_clim_str)
+  names(list_output_obj) <- c("command_download_str", "command_clim_str")
   return(list_output_obj)
 }
 
 ## Run function:
 
 #list_tiles_modis <- c("h10v04,h11v04,h12v04,h13v04,h14v04,h07v06") #tiles for Northenr America, Northern US...
-list_tiles_modis <- c("h12v04,h13v04,h14v04,h07v06") #tiles for Northenr America, Northern US...
+#list_tiles_modis <- c("h12v04,h13v04,h14v04,h07v06") #tiles for Northenr America, Northern US...
 #list_tiles_modis <- c("h11v08,h11v07,h12v07,h12v08,h10v07,h10v08") #tile for Venezuela and surrounding area
 #list_tiles_modis <- c("h08v04,h09v04") #tiles for Oregon #defined above...
+#list_tiles_modis <- c("h09v04,h09v04") #tiles for Oregon #defined above...
 
 #list_tiles_modis <- c("h09v04,h09v04") #tiles for Oregon #defined above...
 #list_tiles_modis <- c("h09v08,h09v07,h08v07,h0706,h08v06,h09v06,h10v06,h08v05,h09v05,h10v05,h11v05,h12v05") #tiles for Central America and Mexico Southern US...
 #list_tiles_modis <- c("h09v09,h10v09,h11v09,h12v09,h13v09,h14v09")
 #list_tiles_modis <-c("h30v10,h31v10,h32v10,h30v11,h31v11") #list("Queensland")
 
-script_path<-"/home/parmentier/Data/IPLANT_project/env_layers_scripts/"
-modis_download_script <- file.path(script_path,"modis_download_05142013.py") # LST modis download python script
-clim_script <- file.path(script_path,"climatology_05142013.py") # LST climatology python script
-grass_setting_script <- file.path(script_path,"grass-setup.R")
-var="TMAX"
-start_year = "2001"
-end_year = "2010"
+#script_path<-"/home/parmentier/Data/IPLANT_project/env_layers_scripts/"
+#modis_download_script <- file.path(script_path,"modis_download_05142013.py") # LST modis download python script
+#clim_script <- file.path(script_path,"climatology_05142013.py") # LST climatology python script
+#grass_setting_script <- file.path(script_path,"grass-setup.R")
+#var="TMAX"
+#start_year = "2001"
+#end_year = "2010"
+#end_year = "2002"
+
 #path on Jupiter
 
-hdfdir =  '/data/project/layers/commons/modis/MOD11A1_tiles' #destination file where hdf files are stored locally after download.
+#hdfdir =  '/data/project/layers/commons/modis/MOD11A1_tiles' #destination file where hdf files are stored locally after download.
 #hdfdir =  '/home/parmentier/Data/IPLANT_project/MOD11A1_tiles'
-download=1
-clim_calc=0
-out_suffix_modis="_05292013"
+#download=0
+#clim_calc=1
+#out_suffix_modis="_05302013"
 
-list_param_download_clim_LST_script <- list(list_tiles_modis,start_year,end_year,hdfdir,
-                                 var,grass_setting_script,modis_download_script, clim_script,
-                                 download,clim_calc,out_suffix_modis)
-names(list_param_download_clim_LST_script)<-c("list_tiles_modis","start_year","end_year","hdfdir",
-                                   "var","grass_setting_script","modis_download_script","clim_script",
-                                   "download","clim_calc","out_suffix_modis")
+#list_param_download_clim_LST_script <- list(list_tiles_modis,start_year,end_year,hdfdir,
+#                                 var,grass_setting_script,modis_download_script, clim_script,
+#                                 download,clim_calc,out_suffix_modis)
+#names(list_param_download_clim_LST_script)<-c("list_tiles_modis","start_year","end_year","hdfdir",
+#                                   "var","grass_setting_script","modis_download_script","clim_script",
+#                                   "download","clim_calc","out_suffix_modis")
 #debug(download_calculate_MODIS_LST_climatology)
 #clim_production_obj <-mclapply(1:2, list_param=list_param_download_clim_LST_script, download_calculate_MODIS_LST_climatology,mc.preschedule=FALSE,mc.cores = 2) #This is the end bracket from mclapply(...) statement
+#clim_production_obj <-lapply(1:2, list_param=list_param_download_clim_LST_script, download_calculate_MODIS_LST_climatology) #,mc.preschedule=FALSE,mc.cores = 2) #This is the end bracket from mclapply(...) statement
 
-download_calculate_MODIS_LST_climatology(1,list_param_download_clim_LST_script)
-
+#download_calculate_MODIS_LST_climatology(1,list_param_download_clim_LST_script)
+#source(file.path(script_path,"covariates_production_temperatures_05302013.R"))
 ### END OF SCRIPT
