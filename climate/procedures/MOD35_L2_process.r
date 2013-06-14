a981dca4bd611619aceb21c6dba966f2eb45d84a
Adam M. Wilson @ pfe <Adam.Wilson@yale.edu>
2013-06-13 20:08:21 -0700

    Adding sensor zenith to quality calculation and improving the filter used to select daily data

--- climate/procedures/MOD35_L2_process.r
+++ climate/procedures/MOD35_L2_process.r
@@ -35,8 +35,8 @@ if ( !is.null(opta$help) )
 
 
 ## default date and tile to play with  (will be overwritten below when running in batch)
-#date="20090129"
-#tile="h17v00"
+date="20090129"
+tile="h17v00"
 platform="pleiades" 
 verbose=T
 
@@ -44,6 +44,7 @@ verbose=T
 date=opta$date  
 tile=opta$tile 
 verbose=opta$verbose  #print out extensive information for debugging?
+
 ## get year and doy from date
 year=format(as.Date(date,"%Y%m%d"),"%Y")
 doy=format(as.Date(date,"%Y%m%d"),"%j")
@@ -53,21 +54,8 @@ if(platform=="pleiades"){
   datadir=paste("/nobackupp4/datapool/modis/MOD35_L2.006/",year,"/",doy,"/",sep="")
   ## path to some executables
   ncopath="/nasa/sles11/nco/4.0.8/gcc/mpt/bin/"
-  swtifpath="/nobackupp1/awilso10/software/heg/bin/swtif"
-  swtifpath="/nobackupp4/pvotava/software/heg/bin/swtif"
-  ## path to swath database
-  db="/nobackupp4/pvotava/DB/export/swath_geo.sql.sqlite3.db"
-  ## specify working directory
-  outdir=paste("/nobackupp1/awilso10/mod35/daily/",tile,"/",sep="")  #directory for separate daily files
-  basedir="/nobackupp1/awilso10/mod35/" #directory to hold files temporarily before transferring to lou
-  setwd(tempdir())
-  ## grass database
-  gisBase="/u/armichae/pr/grass-6.4.2/"
-  ## path to MOD11A1 file for this tile to align grid/extent
-  gridfile=list.files("/nobackupp4/datapool/modis/MOD11A2.005/2009.01.01",pattern=paste(tile,".*[.]hdf$",sep=""),recursive=T,full=T)[1]
-  td=readGDAL(paste("HDF4_EOS:EOS_GRID:\"",gridfile,"\":MODIS_Grid_8Day_1km_LST:LST_Day_1km",sep=""))
-  projection(td)="+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs +datum=WGS84 +ellps=WGS84 "
-}
+#  swtifpath="/nobackupp1/awilso10/software/heg/bin/swtif"
+  swtifpath="/nobackupp4/pvotava/software/heg/2.11/bin/swtif"
   ## path to swath database
   db="/nobackupp4/pvotava/DB/export/swath_geo.sql.sqlite3.db"
   ## specify working directory
@@ -114,14 +102,6 @@ tile_bb=tb[tb$tile==tile,] ## identify tile of interest
 upleft=paste(min(90,tile_bb$lat_max+0.5),max(-180,tile_bb$lon_min-0.5)) #northwest corner
 lowright=paste(max(-90,tile_bb$lat_min-0.5),min(180,tile_bb$lon_max+0.5)) #southeast corner
 
-## vars to process
-vars=as.data.frame(matrix(c(
-  "Cloud_Mask",              "CM",
-  "Quality_Assurance",       "QA"),
-#  "Solar_Azimuth",        "SA",
-#  "Sensor_Zenith",        "SZ"),
-  byrow=T,ncol=2,dimnames=list(1:2,c("variable","varid"))),stringsAsFactors=F)
-
 ## vector of variables expected to be in final netcdf file.  If these are not present, the file will be deleted at the end.
 finalvars=c("CMday","CMnight")
 
@@ -152,29 +132,35 @@ if(verbose) print(paste(nrow(fs)," swath IDs recieved from database and ",length
 for(file in swaths){
   ## Function to generate hegtool parameter file for multi-band HDF-EOS file
   print(paste("Starting file",basename(file)))
-  outfile=paste(tempdir(),"/",basename(file),sep="")
-  ### Get 1km data
+
+  ## vars to process
+km1vars=as.data.frame(matrix(c(
+  "Cloud_Mask",              "CM",
+  "Quality_Assurance",       "QA"),
+  byrow=T,ncol=2,dimnames=list(1:2,c("variable","varid"))),stringsAsFactors=F)
+  km1outfile=paste(tempdir(),"/km1_",basename(file),sep="")
+  ## Get 1km data
   ## First write the parameter file (careful, heg is very finicky!)
-  hdr=paste("NUM_RUNS = ",length(vars$varid),"|MULTI_BAND_HDFEOS:",length(vars$varid),sep="")
+  hdr=paste("NUM_RUNS = ",length(km1vars$varid),"|MULTI_BAND_HDFEOS:",length(km1vars$varid),sep="")
   grp=paste("
 BEGIN
 INPUT_FILENAME=",file,"
 OBJECT_NAME=mod35
-FIELD_NAME=",vars$variable,"|
+FIELD_NAME=",km1vars$variable,"|
 BAND_NUMBER = 1
 OUTPUT_PIXEL_SIZE_X=926.6
 OUTPUT_PIXEL_SIZE_Y=926.6
 # MODIS 1km Resolution
 SPATIAL_SUBSET_UL_CORNER = ( ",upleft," )
 SPATIAL_SUBSET_LR_CORNER = ( ",lowright," )
-#RESAMPLING_TYPE =",ifelse(grepl("Flag|Mask|Quality",vars),"NN","CUBIC"),"
+#RESAMPLING_TYPE =",ifelse(grepl("Flag|Mask|Quality",km1vars),"NN","CUBIC"),"
 RESAMPLING_TYPE =NN
 OUTPUT_PROJECTION_TYPE = SIN
 OUTPUT_PROJECTION_PARAMETERS = ( 6371007.181 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 )
 # projection parameters from http://landweb.nascom.nasa.gov/cgi-bin/QA_WWW/newPage.cgi?fileName=sn_gctp
 ELLIPSOID_CODE = WGS84
 OUTPUT_TYPE = HDFEOS
-OUTPUT_FILENAME = ",outfile,"
+OUTPUT_FILENAME = ",km1outfile,"
 END
 
 ",sep="")
@@ -187,12 +173,50 @@ END
   ## now run the swath2grid tool
   ## write the gridded file
   system(paste(swtifpath," -p ",tempdir(),"/",basename(file),"_MODparms.txt -d  -tmpLatLondir ",tempdir(),sep=""),intern=F,ignore.stderr=F)
+#################
+  ## 5km quality data
+km5vars=as.data.frame(matrix(c(
+  "Solar_Zenith",        "SolZen",
+    "Sensor_Zenith",        "SenZen"),
+    byrow=T,ncol=2,dimnames=list(1:2,c("variable","varid"))),stringsAsFactors=F)
+ km5outfile=paste(tempdir(),"/km5_",basename(file),sep="")
+ 
+  hdr=paste("NUM_RUNS = ",length(km5vars$varid),"|MULTI_BAND_HDFEOS:",length(km5vars$varid),sep="")
+  grp=paste("
+BEGIN
+INPUT_FILENAME=",file,"
+OBJECT_NAME=mod35
+FIELD_NAME=",km5vars$variable,"|
+BAND_NUMBER = 1
+OUTPUT_PIXEL_SIZE_X=926.6
+OUTPUT_PIXEL_SIZE_Y=926.6
+# MODIS 1km Resolution
+SPATIAL_SUBSET_UL_CORNER = ( ",upleft," )
+SPATIAL_SUBSET_LR_CORNER = ( ",lowright," )
+#RESAMPLING_TYPE =",ifelse(grepl("Flag|Mask|Quality",km1vars),"NN","CUBIC"),"
+RESAMPLING_TYPE =CUBIC
+OUTPUT_PROJECTION_TYPE = SIN
+OUTPUT_PROJECTION_PARAMETERS = ( 6371007.181 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 )
+# projection parameters from http://landweb.nascom.nasa.gov/cgi-bin/QA_WWW/newPage.cgi?fileName=sn_gctp
+ELLIPSOID_CODE = WGS84
+OUTPUT_TYPE = HDFEOS
+OUTPUT_FILENAME = ",km5outfile,"
+END
+
+",sep="")
+
+  ## write it to a file
+  cat(c(hdr,grp)    , file=paste(tempdir(),"/",basename(file),"_MODparms.txt",sep=""))
+  ## now run the swath2grid tool
+  ## write the gridded file
+  system(paste(swtifpath," -p ",tempdir(),"/",basename(file),"_MODparms.txt -d  -tmpLatLondir ",tempdir(),sep=""),intern=F,ignore.stderr=F)
+
   print(paste("Finished gridding ", file," for tile ",tile))
 }  #end looping over swaths
 
 ########################
 ## confirm at least one file for this date is present.  If not, quit.
-outfiles=paste(tempdir(),"/",basename(swaths),sep="")
+outfiles=list.files(tempdir(),full=T,pattern=paste(basename(swaths),"$",sep="",collapse="|"))
 if(!any(file.exists(outfiles))) {
   print(paste("########################################   No gridded files for region exist for tile",tile," on date",date))
   q("no",status=0)
@@ -201,16 +225,16 @@ if(!any(file.exists(outfiles))) {
 plot=F
 if(plot){
 i=1
-GDALinfo(outfiles[i])
+system(paste("gdalinfo ",swaths[1]))
 d=brick(
-  raster(paste("HDF4_EOS:EOS_GRID:\"",outfiles[i],"\":mod35:Cloud_Mask_0",sep="")),
-  raster(paste("HDF4_EOS:EOS_GRID:\"",outfiles[i],"\":mod35:Quality_Assurance_0",sep="")),
-  raster(paste("HDF4_EOS:EOS_GRID:\"",outfiles[i],"\":mod35:Sensor_Zenith",sep="")),
-  raster(paste("HDF4_EOS:EOS_GRID:\"",outfiles[i],"\":mod35:Solar_Azimuth",sep="")))
-plot(d)
+  raster(paste("HDF4_EOS:EOS_GRID:\"",outfiles[1],"\":mod35:Cloud_Mask_0",sep="")),
+  raster(paste("HDF4_EOS:EOS_GRID:\"",outfiles[1],"\":mod35:Quality_Assurance_0",sep="")),
+  raster(paste("HDF4_EOS:EOS_GRID:\"",outfiles[26],"\":mod35:Sensor_Zenith",sep="")),
+  raster(paste("HDF4_EOS:EOS_GRID:\"",outfiles[26],"\":mod35:Solar_Zenith",sep=""))
+)
+  plot(d)
 }
-# TODO: import sensor zenith separately and mask out bad pixels
-system(paste("scp ",outfiles[1]," adamw@acrobates.eeb.yale.edu:/data/personal/adamw/projects/interp/tmp/",sep=""))
+#system(paste("scp ",outfiles[1]," adamw@acrobates.eeb.yale.edu:/data/personal/adamw/projects/interp/tmp/",sep=""))
 
 #####################################################
 ## Process the gridded files to align exactly with MODLAND tile and produce a daily summary of multiple swaths
@@ -237,7 +261,7 @@ system(paste("scp ",outfiles[1]," adamw@acrobates.eeb.yale.edu:/data/personal/ad
  
   ## set up temporary grass instance for this PID
   if(verbose) print(paste("Set up temporary grass session in",tf))
-  initGRASS(gisBase=gisBase,gisDbase=tf,SG=as(td,"SpatialGridDataFrame"),override=T,location="mod06",mapset="PERMANENT",home=tf,pid=Sys.getpid())
+  initGRASS(gisBase=gisBase,gisDbase=tf,SG=as(td,"SpatialGridDataFrame"),override=T,location="mod35",mapset="PERMANENT",home=tf,pid=Sys.getpid())
   system(paste("g.proj -c proj4=\"",projection(td),"\"",sep=""),ignore.stdout=T,ignore.stderr=T)
 
   ## Define region by importing one MOD11A1 raster.
@@ -253,42 +277,130 @@ if(platform=="litoria"){
             output="modisgrid",flags=c("overwrite","o"))
   system("g.region rast=modisgrid.1 save=roi --overwrite",ignore.stdout=F,ignore.stderr=F)
 }
+
 ## Identify which files to process
-tfs=basename(swaths)
-## drop swaths that did not produce an output file (typically due to not overlapping the ROI)
-tfs=tfs[tfs%in%list.files(tempdir())]
+tfs=unique(sub("km1_|km5_","",basename(outfiles)))
 #tfs=list.files(tempdir(),pattern="temp.*hdf")
 nfs=length(tfs)
 if(verbose) print(paste(nfs,"swaths available for processing"))
 
 ## loop through scenes and process QA flags
   for(i in 1:nfs){
-     file=paste(tempdir(),"/",tfs[i],sep="")
+     file=paste(tempdir(),"/km1_",tfs[i],sep="")
+     km5file=paste(tempdir(),"/km5_",tfs[i],sep="")
+
+     ## get GRING coordinates of swath to crop raster
+     tswath=swaths[grep(tfs[i],swaths)]
+     system(paste("gdalinfo ",tswath))
+     lat=raster(paste("HDF4_SDS:UNKNOWN:\"",tswath,"\":0",sep=""))
+     lon=raster(paste("HDF4_SDS:UNKNOWN:\"",tswath,"\":1",sep=""))
+     ## HEG Tool reprojection results in large areas of sprious values (regions outside data areas are filled in using the interpolation method)
+     ## need to crop the resulting projected data to eliminate these areas
+     coords=cbind.data.frame(melt(as.matrix(lat))[,1:3],lon=melt(as.matrix(lon))[,3])
+     coords=coords[coords$X1%in%range(coords$X1)|coords$X2%in%range(coords$X2),4:3];colnames(coords)=c("lon","lat")
+     #coords=cbind.data.frame(lat=melt(as.matrix(lat))[,3],lon=melt(as.matrix(lon))[,3])
+     ## crop to big bbox
+#     coords=coords[coords$lat<tile_bb$lat_max+0.5&coords$lat>tile_bb$lat_min-0.5&
+#       coords$lon>tile_bb$lon_min-0.5&coords$lon<tile_bb$lon_max+0.5,]
+#     coordinates(coords)=c("lon","lat")
+#     proj4string(coords)="+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs "
+     ## project to sinusoidal
+     coords2=spTransform(coords,CRS(projection(td)))
+     ## fit alpha hull to draw polygon around region with data
+     ah=ahull(coordinates(coords2),alpha=100000)
+     ah2=ah$x[ah$alpha.extremes,]
+     ah2=ah$edges[,c("x1","y1")]
+  
+     pp = SpatialPolygons(list(Polygons(list(Polygon(coords[c(1:nrow(coords),1),])),1)))
+     proj4string(pp)=projection(td)
+
+     plot(coords,add=F);axis(1);axis(2)
+     plot(d2[[2]],add=F)
+     plot(coords2,add=T);axis(1);axis(2)
+     plot(ah,wpoints=F,add=T,col="red")
+     points(ah2,add=T)
+     
+     glat=as.numeric(do.call(c,strsplit(sub("GRINGPOINTLATITUDE=","",system(paste("gdalinfo ",tswath," | grep GRINGPOINTLATITUDE"),intern=T)),split=",")))
+     glon=as.numeric(do.call(c,strsplit(sub("GRINGPOINTLONGITUDE=","",system(paste("gdalinfo ",tswath," | grep GRINGPOINTLONGITUDE"),intern=T)),split=",")))
+     bb=cbind(c(glon,glon[1]),c(glat,glat[1]))
+     pp = SpatialPolygons(list(Polygons(list(Polygon(bb)),1)))
+     proj4string(pp)="+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs "
+     pp2=spsample(as(pp,"SpatialLines"), 100, type="regular") 
+     pp2=spTransform(pp2,CRS(projection(td)))
+
+     dt=projectRaster(d2[[1]],crs="+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs ")
+     
+     plot(pp,add=F,usePolypath = FALSE);axis(1);axis(2)#(sp.polygons(pp),usePolypath = FALSE)
+     plot(d2[[2]],add=F)
+
+
      ## Cloud Mask
      execGRASS("r.in.gdal",input=paste("HDF4_EOS:EOS_GRID:\"",file,"\":mod35:Cloud_Mask_0",sep=""),
               output=paste("CM1_",i,sep=""),flags=c("overwrite","o")) ; print("")
     ## QA      ## extract first bit to keep only "useful" values of cloud mask
      execGRASS("r.in.gdal",input=paste("HDF4_EOS:EOS_GRID:\"",file,"\":mod35:Quality_Assurance_0",sep=""),
              output=paste("QA_",i,sep=""),flags=c("overwrite","o")) ; print("")
-     ## extract first two bits of cloud mask
-    system(paste("r.mapcalc <<EOF
+    ## Sensor Zenith      ## extract first bit to keep only "low angle" observations
+     execGRASS("r.in.gdal",input=paste("HDF4_EOS:EOS_GRID:\"",km5file,"\":mod35:Sensor_Zenith",sep=""),
+             output=paste("SZ_",i,sep=""),flags=c("overwrite","o")) ; print("")
+    ## Solar Zenith      ## extract first bit to keep only "low angle" observations
+     execGRASS("r.in.gdal",input=paste("HDF4_EOS:EOS_GRID:\"",km5file,"\":mod35:Solar_Zenith",sep=""),
+             output=paste("SoZ_",i,sep=""),flags=c("overwrite","o")) ; print("")
+
+     system(paste("r.mapcalc <<EOF
+                CM_fill_",i," =  if(isnull(CM1_",i,"),1,0)
                 QA_useful_",i," =  if((QA_",i," / 2^0) % 2==1,1,0)
-                CM_cloud_",i," =  if((CM1_",i," / 2^0) % 2==1,(CM1_",i," / 2^1) % 2^2,-9999)
+                SZ_low_",i," =  if(SZ_",i,"<6000,1,0)
+                SoZ_low_",i," =  if(SoZ_",i,"<8500,1,0)
                 CM_dayflag_",i," =  if((CM1_",i," / 2^3) % 2==1,1,0)
-                CMday_",i," = if(QA_useful_",i,"==1&CM_dayflag_",i,"==1,CM_cloud_",i,",-9999)
-                CMnight_",i," = if(QA_useful_",i,"==1&CM_dayflag_",i,"==0,CM_cloud_",i,",-9999)
+                CM_cloud_",i," =  if((CM1_",i," / 2^0) % 2==1,(CM1_",i," / 2^1) % 2^2,-9999)
+                CMday_",i," = if(SoZ_low_",i,"==1&SZ_low_",i,"==1&QA_useful_",i,"==1&CM_dayflag_",i,"==1,CM_cloud_",i,",-9999)
+                CMnight_",i," = if(SZ_low_",i,"==1&QA_useful_",i,"==1&CM_dayflag_",i,"==0,CM_cloud_",i,",-9999)
 EOF",sep=""))
-                                        #Pclear1_",i," = if(CM_cloud_",i,"==0,0,if(CM_cloud_",i,"==1,66,if(CM_cloud_",i,"==2,95,if(CM_cloud_",i,"==3,99,-9999))))
-                                        #CM_path_",i," =   ((CM1_",i," / 2^6) % 2^2) 
 
-#     execGRASS("r.null",map=paste("Pclearday_",i,sep=""),setnull="-9999")
-#     execGRASS("r.null",map=paste("Pclearnight_",i,sep=""),setnull="-9999")
+#     CM_dayflag_",i," =  if((CM1_",i," / 2^3) % 2==1,1,0)
+#       CM_dscore_",i," =  if((CM_dayflag_",i,"==0|isnull(CM1_",i,")),0,if(QA_useful_",i,"==0,1,if(SZ_",i,">=6000,2,if(SoZ_",i,">=8500,3,4))))
+#         CM_nscore_",i," =  if((CM_dayflag_",i,"==1|isnull(CM1_",i,")),0,if(QA_useful_",i,"==0,1,if(SZ_",i,">=6000,2,4)))
 
+          ## set null values
      execGRASS("r.null",map=paste("CMday_",i,sep=""),setnull="-9999")
      execGRASS("r.null",map=paste("CMnight_",i,sep=""),setnull="-9999")
-   
-    
+
+     drawplot=F
+     if(drawplot){
+       d2=stack(
+         raster(readRAST6(paste("QA_useful_",i,sep=""))),
+         raster(readRAST6(paste("CM1_",i,sep=""))),
+         raster(readRAST6(paste("CM_cloud_",i,sep=""))),
+         raster(readRAST6(paste("CMday_",i,sep=""))),
+         raster(readRAST6(paste("CMnight_",i,sep=""))),
+         raster(readRAST6(paste("CM_fill_",i,sep=""))),
+         raster(readRAST6(paste("SoZ_",i,sep=""))),
+         raster(readRAST6(paste("SZ_",i,sep="")))
+         )
+       plot(d2[[2]],add=F)
+       plot(coords2,pch=16,cex=.2,add=T)
+       plot(pp,add=F,usePolypath = FALSE)#(sp.polygons(pp),usePolypath = FALSE)
+     }
+       
+     
  } #end loop through sub daily files
+
+## select lowest view angle
+## use r.series to find minimum
+system("r.series input=`g.mlist rast pat=\"SZ_[0-9]*$\" sep=,` output=SZ_min method=min_raster")
+
+## select cloud observation with lowest sensor zenith for day and night
+system(
+paste("r.mapcalc <<EOF
+              CMday_daily=",paste(paste("if((SZ_min+1)==",1:nfs,",CMday_",1:nfs,",",sep="",collapse=" "),"null()",paste(rep(")",times=nfs),sep="",collapse="")),"
+              CMnight_daily=",paste(paste("if((SZ_min+1)==",1:nfs,",CMnight_",1:nfs,",",sep="",collapse=" "),"null()",paste(rep(")",times=nfs),sep="",collapse=""))
+))
+
+d=brick(lapply(1:nfs,function(i) raster(readRAST6(paste("CMnight_",i,sep="")))))
+levelplot(d)
+
+plot(raster(readRAST6("CMnight_daily")))
 
 #### Now generate daily minimum p(clear)
   system(paste("r.mapcalc <<EOF
