bf9dd6e2a1294273d1d2d94b6e0d1c5d51246e0d
Benoit Parmentier <parmentier@nceas.ucsb.edu>
2013-09-23 10:45:00 -0700

    running gwr fusion, 0-30% holdout for multi-time scale paper, OR

--- climate/research/oregon/interpolation/master_script_temp.R
+++ climate/research/oregon/interpolation/master_script_temp.R
@@ -10,7 +10,7 @@
 #STAGE 5: Output analyses: assessment of results for specific dates...
 #
 #AUTHOR: Benoit Parmentier                                                                       
-#DATE: 09/18/2013                                                                                 
+#DATE: 09/23/2013                                                                                 
 
 #PROJECT: NCEAS INPLANT: Environment and Organisms --TASK#363, TASK$568--   
 
@@ -80,16 +80,16 @@ met_stations_outfiles_obj_file<-"/data/project/layers/commons/data_workflow/outp
 #met_stations_outfiles_obj_file<-"met_stations_outfiles_obj_gam_CAI__365d_gam_CAI_lst_comb3_08252013.RData"
 
 var<-"TMAX" # variable being interpolated
-out_prefix<-"_365d_gwr_CAI_lst_comb3_09182013"                #User defined output prefix
-out_suffix<-"_OR_09182013"                                       #Regional suffix
+out_prefix<-"_365d_gwr_fus_lst_comb3_09232013"                #User defined output prefix
+out_suffix<-"_OR_09232013"                                       #Regional suffix
 out_suffix_modis <-"_05302013"                       #pattern to find tiles produced previously     
 
 #interpolation_method<-c("gam_fusion","gam_CAI","gam_daily") #other otpions to be added later
 #interpolation_method<-c("gam_CAI") #other otpions to be added later
 #interpolation_method<-c("gam_fusion") #other otpions to be added later
 #interpolation_method<-c("kriging_fusion") #other otpions to be added later
-#interpolation_method<-c("gwr_fusion") #other otpions to be added later
-interpolation_method<-c("gwr_CAI") #other otpions to be added later
+interpolation_method<-c("gwr_fusion") #other otpions to be added later
+#interpolation_method<-c("gwr_CAI") #other otpions to be added later
 #interpolation_method<-c("kriging_CAI") 
 
 #interpolation_method<-c("gam_daily") #other otpions to be added later
@@ -254,7 +254,7 @@ seed_number_month <- 100
 nb_sample_month <-1           #number of time random sampling must be repeated for every hold out proportion
 step_month <-0.1         
 constant_month <- 0             #if value 1 then use the same samples as date one for the all set of dates
-prop_minmax_month <-c(0.4,0.7)  #if prop_min=prop_max and step=0 then predictions are done for the number of dates...
+prop_minmax_month <-c(0,0.1)  #if prop_min=prop_max and step=0 then predictions are done for the number of dates...
 
 #dates_selected<-c("20100101","20100102","20100103","20100901") # Note that the dates set must have a specific format: yyymmdd
 #dates_selected<-c("20100101","20100102","20100301","20100302","20100501","20100502","20100701","20100702","20100901","20100902","20101101","20101102")
