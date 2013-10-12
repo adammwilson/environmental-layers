5d05bb612822169910d888591c42b7bab0b8a003
Benoit Parmentier <parmentier@nceas.ucsb.edu>
2013-10-12 10:45:00 -0700

    running gam daily multiple holdout 10-70% every 2 days for single time scale paper

--- climate/research/oregon/interpolation/master_script_temp.R
+++ climate/research/oregon/interpolation/master_script_temp.R
@@ -81,9 +81,9 @@ met_stations_outfiles_obj_file<-"/data/project/layers/commons/data_workflow/outp
 
 var<-"TMAX" # variable being interpolated
 #out_prefix<-"_365d_gam_cai_lst_comb3_10102013"                #User defined output prefix
-out_prefix<-"_365d_kriging_daily_mults1_lst_comb3_10112013"                #User defined output prefix
+out_prefix<-"_365d_gam_daily_mults1_lst_comb3_10122013"                #User defined output prefix
 
-out_suffix<-"_OR_10112013"                                       #Regional suffix
+out_suffix<-"_OR_10122013"                                       #Regional suffix
 out_suffix_modis <-"_05302013"                       #pattern to find tiles produced previously     
 
 #interpolation_method<-c("gam_fusion","gam_CAI","gam_daily") #other otpions to be added later
@@ -94,8 +94,8 @@ out_suffix_modis <-"_05302013"                       #pattern to find tiles prod
 #interpolation_method<-c("gwr_CAI") #other otpions to be added later
 #interpolation_method<-c("kriging_CAI") 
 
-#interpolation_method<-c("gam_daily") #other otpions to be added later
-interpolation_method<-c("kriging_daily") #other otpions to be added later
+interpolation_method<-c("gam_daily") #other otpions to be added later
+#interpolation_method<-c("kriging_daily") #other otpions to be added later
 #interpolation_method<-c("gwr_daily") #other otpions to be added later
 
 #out_path<-"/home/parmentier/Data/IPLANT_project/Oregon_interpolation/Oregon_03142013/output_data"
@@ -311,9 +311,9 @@ interp_method2 <- NULL #other options are "gwr" and "kriging"
 #interp_method2 <- "gam" #other options are "gwr" and "kriging"
 #list_models<-c("y_var ~ lat*lon + elev_s")
 
-#list_models<-c("y_var ~ s(lat,lon) + s(elev_s)")
+list_models<-c("y_var ~ s(lat,lon) + s(elev_s)")
 
-list_models<-c("y_var ~ lat*lon + elev_s") #,
+#list_models<-c("y_var ~ lat*lon + elev_s") #,
 #               "y_var ~ lat*lon + elev_s + N_w",
 #               "y_var ~ lat*lon + elev_s + E_w",
 #               "y_var ~ lat*lon + elev_s + LST",
