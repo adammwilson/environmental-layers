948b9bf3fda9df13c00ca84770ada466fda8b501
Adam M. Wilson <adam.wilson@yale.edu>
2014-03-26 14:29:29 -0400

    Updating compile script to include VSNR processing to remove MODIS orbit bands from cloud frequency data

--- climate/research/cloud/MOD09/ee/ee_compile.R
+++ climate/research/cloud/MOD09/ee/ee_compile.R
@@ -1,10 +1,11 @@
 ###  Script to compile the monthly cloud data from earth engine into a netcdf file for further processing
 
-library(raster)
+library(rasterVis)
 library(doMC)
 library(multicore)
 library(foreach)
 library(mgcv)
+library(RcppOctave)
 registerDoMC(12)
 
 
@@ -13,8 +14,16 @@ registerDoMC(12)
 
 setwd("~/acrobates/adamw/projects/cloud")
 
+datadir="/mnt/data2/projects/cloud/"
+
+mpath="/home/adamw/acrobates/adamw/projects/environmental-layers/climate/research/cloud/MOD09/vsnr/"
+.O$addpath(mpath)
+## download data from google drive
+
+
+
 ##  Get list of available files
-df=data.frame(path=list.files("data/mcd09",pattern="*.tif$",full=T,recur=T),stringsAsFactors=F)
+df=data.frame(path=list.files(paste(datadir,"mcd09ee",sep="/"),pattern="*.tif$",full=T,recur=T),stringsAsFactors=F)
 df[,c("month","sensor")]=do.call(rbind,strsplit(basename(df$path),"_|[.]|-"))[,c(5,4)]
 df$date=as.Date(paste(2013,"_",df$month,"_15",sep=""),"%Y_%m_%d")
 
@@ -37,13 +46,13 @@ rasterOptions(tmpdir=tmpfs,overwrite=T, format="GTiff",maxmemory=1e9)
 rerun=T  # set to true to recalculate all dates even if file already exists
 
 
-jobs=expand.grid(month=1:12,sensor=c("MOD09","MYD09"))
+jobs=expand.grid(month=1:12,sensor=c("MOD09GA","MYD09GA"))
 i=1
 
 #jobs=jobs[jobs$sensor=="MYD09",]
 
 
-## Loop over data to mosaic tifs and remove problematic data at poles 
+## Loop over data to mosaic tifs, compress, and add metadata
     foreach(i=1:nrow(jobs)) %dopar% {
         ## get month
         m=jobs$month[i]
@@ -51,44 +60,135 @@ i=1
         print(date)
         ## get sensor
         s=jobs$sensor[i]
+        s2=sub("GA","",s)
+
         ## Define output and check if it already exists
-        tvrt=paste(tmpfs,"/",s,"_",sprintf("%02d", m),".vrt",sep="")
-        ttif1=paste("data/mcd09tif/",s,"_",sprintf("%02d", m),".tif",sep="")
+        tvrt=paste(tmpfs,"/",s2,"_",sprintf("%02d", m),".vrt",sep="")
+        ttif1=paste(tmpfs,"/",s2,"_",sprintf("%02d", m),".tif",sep="")
+        ttif2=paste(datadir,"/mcd09tif/",s2,"_",sprintf("%02d", m),".tif",sep="")
 
         ## check if output already exists
         if(!rerun&file.exists(ttif1)) return(NA)
         ## build VRT to merge tiles
-        system(paste("gdalbuildvrt ",tvrt," ",paste(df$path[df$month==m&df$sensor==s],collapse=" ")))
+        proj="'+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs'"
+        system(paste("gdalbuildvrt -b 1 -b 2 ",tvrt," ",paste(df$path[df$month==m&df$sensor==s],collapse=" ")))
+        ## Merge to geotif in temporary directory
+        ## specify sourc projection because it gts it slightly wrong by default
+        ops=paste("-s_srs ",proj,"  -t_srs 'EPSG:4326' -multi -srcnodata 255 -ot Byte -srcnodata -128 -dstnodata 255 -r bilinear -te -180 -90 180 90 -tr 0.008333333333333 -0.008333333333333",
+            "-co BIGTIFF=YES -ot Byte --config GDAL_CACHEMAX 500 -wm 500 -wo NUM_THREADS:10 -co COMPRESS=LZW -co PREDICTOR=2")
+        system(paste("gdalwarp -overwrite ",ops," ",tvrt," ",ttif1))
 
-       ## 
-        mod=stack(tvrt)
-        names(mod)=c("cf","cfsd","nobs","pobs")
-
-        ## mask no data regions (with less than 1 observation per day within that month)
-        biasf=function(cf,cfsd,nobs,pobs) {
-            ## drop data in areas with nobs<1 or pobs<=50 or where sd=0 and cf=0 or 50 (some polar regions)
-            drop=nobs<=0|pobs<=50|(cf==0&cfsd==0)|(cf==50&cfsd==0)
-            cf[drop]=NA
-            cfsd[drop]=NA
-            return(c(cf=cf,cfsd=cfsd,nobs=nobs,pobs=pobs))}
-                
-        mod2=overlay(mod,fun=biasf,unstack=TRUE,filename=ttif1,format="GTiff",
-            dataType="INT1U",overwrite=T,NAflag=255, options=c("COMPRESS=LZW","BIGTIFF=YES"))
-
+        ## Compress file and add metadata tags
+        ops2=paste("-ot Byte -co COMPRESS=LZW -co PREDICTOR=2 -stats")
         tags=c(paste("TIFFTAG_IMAGEDESCRIPTION='Monthly Cloud Frequency for 2000-2013 extracted from C5 MODIS ",s,"GA PGE11 internal cloud mask algorithm (embedded in state_1km bit 10).",
             "The daily cloud mask time series were summarized to mean cloud frequency (CF) by calculating the proportion of cloudy days. ",
-            "Band Descriptions: 1) Mean Monthly Cloud Frequency 2) Four Times the Standard Deviation of Mean Monthly Cloud Frequency",
-            " 3) Mean number of daily observations for each pixel 4) Proportion of days with at least one observation '"),
+            "Band Descriptions: 1) Mean Monthly Cloud Frequency 2) Standard Deviation of Mean Monthly Cloud'"),
               "TIFFTAG_DOCUMENTNAME='Collection 5 ",s," Cloud Frequency'",
               paste("TIFFTAG_DATETIME='2013",sprintf("%02d", m),"15'",sep=""),
               "TIFFTAG_ARTIST='Adam M. Wilson (adam.wilson@yale.edu)'")
-        system(paste("/usr/local/src/gdal-1.10.0/swig/python/scripts/gdal_edit.py ",ttif1," ",paste("-mo ",tags,sep="",collapse=" "),sep=""))
-        
-        rm(mod,mod2)
-        writeLines(paste("Month:",m," Sensor:",s," Finished"))
+        system(paste("gdal_translate  ",ops2," ",paste("-mo ",tags,sep="",collapse=" ")," ",ttif1," ",ttif2))
 
+        ## delete temporary files
+        file.remove(tvrt,ttif1)
+        writeLines(paste("Month:",m," Sensor:",s," Finished"))
     }
 
+
+#########################################
+####  Bias correction functions
+
+fgabor=function(d,theta=-15,x=200,y=5){
+    thetaR=(theta*pi)/180
+    cds=expand.grid(x=(1:nrow(d))-round(nrow(d)/2),y=(1:ncol(d))-round(ncol(d)/2))
+    sigma_x=x
+    sigma_y=y
+    lambda=0
+    tpsi=0
+    x_theta=cds[,"x"]*cos(thetaR)+cds[,"y"]*sin(thetaR);
+    y_theta=-cds[,"x"]*sin(thetaR)+cds[,"y"]*cos(thetaR);
+    n=max(cds)
+    gb= 1/(2*pi*sigma_x *sigma_y) * exp(-.5*(x_theta^2/sigma_x^2+y_theta^2/sigma_y^2))*cos(2*pi/n*lambda*x_theta+tpsi);
+    gb2=1e-2*gb/max(gb); #Normalization
+    psi=d
+    values(psi)=matrix(gb2,ncol=ncol(d))
+    return(psi)
+}
+  
+     
+vsnr=function(d,gabor,alpha=10,p=2,epsilon=0,prec=5e-3,maxit=100,C1=1){
+    ## Process with VSNR
+    dt=.O$VSNR(as.matrix(d),epsilon,p,as.matrix(gabor),alpha,maxit+1,prec,1,argout = c("u", "Gap", "Primal","Dual","EstP","EstD"));
+    ## Create spatial objects from VSNR output
+    dc=d;
+    values(dc)=round(as.vector(t(dt$u)))
+    dc[dc<0]=0
+#    EstP=d;
+#    values(EstP)=as.numeric(t(dt$EstP))
+#    EstD1=d;
+#    values(EstD1)=as.numeric(t(dt$EstD[,,1]))
+#    EstD2=d;
+#    values(EstD2)=as.numeric(t(dt$EstD[,,2]))
+    dif=d-dc#overlay(d,dc,fun=function(x,y) as.integer(x-y))
+    res=stack(list(d=d,dc=dc,dif=dif)) #,EstP=EstP,EstD1=EstD1,EstD2=EstD2
+#    rm(dt,dc,EstP,dif)
+    return(res)
+}
+
+
+######################################
+## Run the correction functions
+###  Subset equitorial region to correct orbital banding
+
+df2=list.files(paste(datadir,"/mcd09tif",sep=""),full=T,pattern="[0-9][.]tif$")
+i=1
+
+### build table of tiles to process
+tsize=10
+exts=data.frame(expand.grid(xmin=seq(-180,180-tsize,by=tsize),ymin=seq(-30,30-tsize,by=tsize)))
+exts=data.frame(expand.grid(xmin=seq(-20,50-tsize,by=tsize),ymin=seq(-30,30-tsize,by=tsize)))
+#exts=data.frame(expand.grid(xmin=seq(-10,20-tsize,by=tsize),ymin=seq(0,30-tsize,by=tsize)))
+
+exts$xmax=exts$xmin+tsize
+exts$ymax=exts$ymin+tsize
+
+getbias=function(file=df2[i],extent,writefile=NULL,...){
+    ## extract the tile
+    d=crop(raster(file),extent)
+    ## skip null tiles
+    if(is.null(d@data@values)) return(NULL)
+    ## make the gabor kernel
+    psi=fgabor(d,theta=-15,x=500,y=3)
+    ## run the correction
+    res=vsnr(d,gabor=psi,alpha=1,p=2,epsilon=1,prec=5e-3,maxit=100,C=1)
+    if(!is.null(writefile)) writeRaster(res,file=writefile,overwrite=T,datatype='INT1S',options=c("COMPRESS=LZW", "PREDICTOR=2"),NAflag=-127)
+    return(res)
+}
+
+### Process the tiles
+foreach(ti=1:nrow(exts)) %dopar% {
+    writeLines(paste("Starting: ",ti))
+    textent=extent(exts$xmin[ti],exts$xmax[ti],exts$ymin[ti],exts$ymax[ti])
+    res=getbias(df2[i],extent=textent,writefile=paste("tmp/test_",ti,".tif",sep=""))
+}
+
+
+## mosaic the tiles
+system("ls tmp/test_[0-9]*[.]tif")
+system("rm tmp/test2.tif; gdal_merge.py -n 255 -ot Byte -co COMPRESS=LZW -co PREDICTOR=2 'tmp/test_[0-9]*[.]tif' -o tmp/test2.tif")
+
+res=brick("tmp/test2.tif")
+names(res)=c("d","dc","dif")
+
+p2=levelplot(res[["dif"]],col.regions=rainbow(100,start=.2),cuts=99,margin=F,maxpixels=1e6);print(p2)
+
+p1=levelplot(stack(list(Original=res[["d"]],Corrected=res[["dc"]])),col.regions=grey.colors(100,start=0),cuts=99,maxPixels=1e7);print(p1)
+
+
+        
+    print(c(p1,p2,merge.legends=T))
+
+
+       
 
 ## Create combined (MOD+MYD) uncorrected mean CF
 #    foreach(i=1:12) %dopar% {
@@ -120,8 +220,13 @@ foreach(i=1:nrow(jobs)) %dopar% {
         date=df$date[df$month==m][1]
         print(date)
 
+        ttif1=paste(datadir,"/mcd09tif/",s,"_",sprintf("%02d", m),".tif",sep="")
         ttif2=paste(tmpfs,"/",s,"_",m,"_wgs84.tif",sep="")
         ncfile=paste("data/mcd09nc/",s,"_",m,".nc",sep="")
+
+        ## 
+        mod=stack(ttif1)
+        names(mod)=c("cf","cfsd","nobs","pobs")
 
         ## set up processing chunks
         nrw=nrow(mod)
@@ -129,6 +234,33 @@ foreach(i=1:nrow(jobs)) %dopar% {
         nrwg=seq(1,nrw,by=nby)
         writeLines(paste("Processing ",length(nrwg)," groups and",nrw,"lines"))
 
+        output=mclapply(nrwg,function(ti){
+            ## Extract focal areas
+            nr=51
+            nc=101
+            ngb=c(nr,nc)
+            vals_cf=getValuesFocal(mod[[c("cf","pobs")]],ngb=ngb,row=ti,nrows=nby)
+            vals_obs=getValuesFocal(obs,ngb=ngb,row=ti,nrows=nby)
+            ## extract bias
+      bias=raster(matrix(do.call(rbind,lapply(1:nrow(vals_cf),function(i) {
+#          if(i%in%round(seq(1,nrow(vals_cf),len=100))) print(i)
+          tobs=vals_obs[i,]  #vector of indices
+          tval=vals_cf[i,]    # vector of values
+          lm1=lm(tval~tobs,na.rm=T)
+          dif=round(predict(lm1)-predict(lm1,newdata=data.frame(tobs=median(tobs,na.rm=T))))
+          return(dif)  
+            })),nrow=nby,ncol=ncol(d),byrow=T))     # turn it back into a raster
+        ## update raster and write it
+        extent(bias)=extent(d[ti:(ti+nby-1),1:ncol(d),drop=F])
+        projection(bias)=projection(d)
+        NAvalue(bias) <- 255
+        writeRaster(bias,file=paste("data/bias/tiles/bias_",ti,".tif",sep=""),
+                                  format="GTiff",dataType="INT1U",overwrite=T,NAflag=255) #,options=c("COMPRESS=LZW","ZLEVEL=9")
+    print(ti)
+  }
+)
+
+        
 
         modpts=sampleRandom(cmod, size=10000, na.rm=TRUE, xy=T, sp=T)
         rm(cmod)  #remove temporary raster to save space
